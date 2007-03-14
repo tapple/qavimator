c7f76b760d634add49e2df2bb0d56265823ec538
zi_ree <zi_ree@50a1c88a-1c17-0410-905d-80aec70f0047>
2007-03-14 13:17:50 +0000

    fixed number of frames display on load (oops 2)
    git-svn-id: https://qavimator.svn.sourceforge.net/svnroot/qavimator@163 50a1c88a-1c17-0410-905d-80aec70f0047

--- src/timelineview.cpp
+++ src/timelineview.cpp
@@ -36,8 +36,8 @@ TimelineView::TimelineView(QWidget* parent,const char* name,WFlags f) : QFrame(p
 
   view=new QScrollView(this,"timeline_inner_view",WNoAutoErase|WStaticContents);
 
-//  view->setVScrollBarMode(QScrollView::AlwaysOff);
-//  view->setHScrollBarMode(QScrollView::AlwaysOff);
+  view->setVScrollBarMode(QScrollView::AlwaysOff);
+  view->setHScrollBarMode(QScrollView::AlwaysOff);
 
   timeline=new Timeline(view->viewport(),"timeline");
 
