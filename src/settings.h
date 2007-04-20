789b5da30555cfaefc6e9cda592e280166043e24
zi_ree <zi_ree@50a1c88a-1c17-0410-905d-80aec70f0047>
2007-04-20 11:42:33 +0000

    forgot to add settings.h/.cpp O.o oops
    git-svn-id: https://qavimator.svn.sourceforge.net/svnroot/qavimator@182 50a1c88a-1c17-0410-905d-80aec70f0047

---
+++ src/settings.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Zi Ree   *
+ *   Zi Ree @ SecondLife   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+/**
+	@author Zi Ree <Zi Ree @ Second Life>
+*/
+
+class Settings
+{
+  public:
+    Settings();
+    ~Settings();
+
+    static void setFog(bool on);
+    static bool fog();
+    static void setTranslucentFloor(bool on);
+    static bool translucentFloor();
+};
+
+#endif
